f46418479e334502ae460150d8eb189c18d880eb
Dima <65868322+byCodeXp@users.noreply.github.com>
2021-04-21 15:41:47 +0300

    service classes

--- Service/IService.cs
+++ Service/IService.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.Text;
+﻿using System.ServiceModel;
 
 namespace Service
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService" in both code and config file together.
-    [ServiceContract]
+    public interface IServiceCallback
+    {
+        [OperationContract]
+        void OnCallback();
+    }
+
+    [ServiceContract(CallbackContract = typeof(IServiceCallback))]
     public interface IService
     {
         [OperationContract]
-        void DoWork();
+        void Start();
     }
 }
--- Service/Service.svc.cs
+++ Service/Service.svc.cs
@@ -1,18 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
 using System.ServiceModel;
-using System.Text;
+using System.Timers;
 
 namespace Service
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service.svc or Service.svc.cs at the Solution Explorer and start debugging.
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Reentrant)]
     public class Service : IService
     {
-        public void DoWork()
+        public static IServiceCallback Callback;
+        public static Timer Timer;
+
+        public void Start()
         {
+            Console.WriteLine("> Session opened at {0}", DateTime.Now);
+            Callback = OperationContext.Current.GetCallbackChannel<IServiceCallback>();
+
+            Timer = new Timer(1000);
+            Timer.Elapsed += OnTimerElapsed;
+            Timer.Enabled = true;
+        }
+
+        void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            Callback.OnCallback();
         }
     }
 }
