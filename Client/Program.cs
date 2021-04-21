3aec73ac9fc0926f2f15f4f20671b540ba353241
Dima <65868322+byCodeXp@users.noreply.github.com>
2021-04-21 15:47:33 +0300

    client work

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,16 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Client.ServiceReference;
+using System;
+using System.ServiceModel;
 
 namespace Client
 {
+    public class ServiceCallback : IServiceCallback
+    {
+        public void OnCallback()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            ServiceCallback callback = new ServiceCallback();
 
+            InstanceContext instanceContext = new InstanceContext(callback);
+
+            ServiceClient client = new ServiceClient(instanceContext);
+
+            client.Start();
+
+            Console.ReadLine();
         }
     }
 }
